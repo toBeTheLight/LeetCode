6df50a527365fa2de834856da22ac7b1cccac33c
toBeTheLight <yl1991web@126.com>
2017-07-12 21:43:37 +0800

    add:#53

--- js/#53 Maximum Subarray.js
+++ js/#53 Maximum Subarray.js
@@ -6,8 +6,9 @@
  */
 
 // error 1.一个超时的答案，遍历次数太多了。
-// 考虑能不能直接单次遍历直接拿和做对比而不需要找到确定的子串
-var maxSubArray = function(nums) {
+// 考虑能不能直接单次遍历直接拿和做对比而不需要找到确定的子串。
+// 之前有考虑从两侧向中间集中找最大子串，但是两侧值的大小比较不够全面，会有问题。
+let maxSubArray = function(nums) {
   let len = nums.length
   if(len == 1) return nums[0]
   let sumArr = []
@@ -25,3 +26,20 @@ var maxSubArray = function(nums) {
   sumArr.sort((a, b) => b - a)
   return sumArr[0]
 };
+
+
+// 2.先上一个别人的
+let maxSubArray = function(nums) {
+  let max = nums[0]
+  let sum = 0
+  nums.forEach((item, index, array) => {
+    sum += item
+    if (sum > max) {
+      max = sum
+    }
+    if (sum < 0) {
+      sum = 0
+    }
+  })
+  return max
+}
